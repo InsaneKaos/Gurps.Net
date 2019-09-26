fa860961b0e10b4ff6c28f7352f1d365f4e7368a
Christian Ohlberg <ChristianOhlberg@gmx.de>
2019-09-26 23:10:33 +0200

    Api added

---
+++ Gurps.Net.Api/Controllers/V1/CORE/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gurps.Net.Api.Controllers.V1.CORE
+{
+    [Produces("application/json")]
+    [Route("api/gurps.net/v1")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        [Route("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await Task.FromResult("Got it"));
+        }
+    }
+}
